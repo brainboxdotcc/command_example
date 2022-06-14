853ed7f781e5e65528004f1f9f05d62a989afba9
Phil B <info@commandserver.org>
2022-06-14 14:51:34 +0200

    added a view fields to /info command

--- src/commands/info.cpp
+++ src/commands/info.cpp
@@ -9,7 +9,10 @@ void handle_info(dpp::cluster& bot, const dpp::slashcommand_t& event) {
 			dpp::embed().
 			set_color(dpp::colors::yellow).
 			set_title("My awesome D++ bot").
-			set_description("A real bot would have something witty here...")
+			set_description("A real bot would have something witty here...").
+			add_field("Uptime", bot.uptime().to_string(), true).
+			add_field("Users", std::to_string(dpp::get_user_count()), true).
+			add_field("Library Version", fmt::format("[{}]({})", dpp::utility::version(), "https://github.com/brainboxdotcc/DPP"), true)
 		)
 	);
 }
